7d3e5113548765e641d7e55594a91b326520adac
OLESYA MILLER <olesyam.miller@gmail.com>
2019-12-03 03:55:38 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,11 +1,23 @@
-# require modules here
+require "yaml"
+require 'pry'
 
-def load_library
-  # code goes here
+def load_library(file_path)
+   
+  emoticons = YAML.load_file(file_path)
+  new_hash = {:get_emoticon => {},:get_meaning => {}}
+  
+  emoticons.each do |key, value| 
+    new_hash[:get_meaning][value[1]] = key
+    new_hash[:get_emoticon][value[0]] = value[1]
+    #binding.pry
+  end
+  new_hash
 end
 
-def get_japanese_emoticon
+def get_japanese_emoticon(yaml_file, emoticon)
+  load_library(yaml_file)
   # code goes here
+  binding.pry 
 end
 
 def get_english_meaning
