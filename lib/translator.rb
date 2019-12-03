1f3daa2466d832a4677a55296834cc9c605522d7
OLESYA MILLER <olesyam.miller@gmail.com>
2019-12-03 04:27:55 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -26,7 +26,7 @@ def get_japanese_emoticon(yaml_file, emoticon)
 end
 
 def get_english_meaning(yaml_file, emoticon)
-  load_library(yaml_file)[]
+  load_library(yaml_file)[:get_meaning][emoticon]
 end
 
 
